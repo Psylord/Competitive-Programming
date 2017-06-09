cd4b5766182c79f955df392062b0f7458e76431a
Jai Yukesh R <ajayyukesh7@gmail.com>
2017-06-10 01:05:37 +0530

    Add files via upload

---
+++ SPOJ/AGGRCOW.java
@@ -0,0 +1,35 @@
+import java.util.*;
+/**
+ * Created by yukesh on 6/10/2017.
+ */
+public class AGGRCOW {
+
+        public static void main(String[] args) {
+            new AGGRCOW().solve();
+        }
+
+        public void solve() {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int m = sc.nextInt();
+                int n = sc.nextInt();
+                for (int i = Math.max(2, m); i <= n; i++) {
+                    if (isPrime(i)) {
+                        System.out.println(i);
+                    }
+                }
+                System.out.println();
+            }
+        }
+
+        public boolean isPrime(int num) {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
